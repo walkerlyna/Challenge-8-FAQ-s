60e637535137f55bba2afcf1d3033dbeccedfad0
Walkerlyna <sotomanriquez36@gmail.com>
2024-02-14 09:25:48 -0700

    start

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,6 @@
+
+export const Faqs = () => {
+  return (
+    <div>Faqs</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Faqs } from './components/Faqs'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Faqs />
+  </React.StrictMode>,
+)
